27f7807976fe86efd6e22cedd1dfbb70a54e11fb
Sean Martin <sean@seanmartinstudio.com>
2023-03-20 11:29:26 -0700

    adds green stroke for squares and black storke for line reset

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let squareSize1 = 100;
 let squareSize2 = 100;
-let c1 = 255;
-let c2 = 0;
+let lineX = 100;
+let lineY = 100;
 
 
 function setup() {
@@ -12,15 +12,23 @@ function setup() {
 function whiteSquare() {
   noFill()
   strokeWeight(4)
-  stroke(color(c1))
+  stroke(color(0, 255, 0))
   square(0, 0, squareSize1)
 }
 
 function blackSquare() {
   noFill()
   strokeWeight(4)
-  stroke(color(c2))
+  stroke(color(0))
   square(0, 0, squareSize2)
+
+  strokeWeight(4)
+  stroke(0, 255, 0)
+  line(0, 0, 0, lineY)
+
+  strokeWeight(4)
+  stroke(0, 255, 0)
+  line(lineX, 0, 0, 0)
 }
 
 function draw() {
@@ -32,14 +40,22 @@ function draw() {
   if(squareSize1 > width) {
     blackSquare()
     squareSize2 += 15
+    lineX += 17
+    lineY += 17
+  
   }
 
     if(squareSize1 && squareSize2 > width) {
       squareSize1 = 100
       squareSize2 = 100
-      // alpha1 -= 50
-      // alpha2 += 50
+      strokeWeight(4)
+      stroke(0)
+      line(100, 0, 600, 0)
+      line(0, 100, 0, 600)
+      
     }
+
+
 
     
     
