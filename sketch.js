2c443347edba25d73d8fa7c4e3f6677bbe5b601e
Sean Martin <sean@seanmartinstudio.com>
2023-03-20 10:08:02 -0700

    added alpha values to draw()

--- sketch.js
+++ sketch.js
@@ -1,32 +1,36 @@
 let squareSize1 = 100;
 let squareSize2 = 100;
-let alphaWhite = 0;
-let alphaBlack = 0;
+let c1 = 255;
+let c2 = 0;
 
 function setup() {
   createCanvas(600, 600);
   background(0);
 }
 
-function whiteSquare(alphaWhite) {
+function whiteSquare() {
   noFill()
   strokeWeight(4)
-  stroke(color(255, alphaWhite))
+  stroke(color(c1))
   square(0, 0, squareSize1)
 }
 
-function blackSquare(alphaBlack) {
+function blackSquare() {
   noFill()
   strokeWeight(4)
-  stroke(color(0, alphaBlack))
+  stroke(color(c2))
   square(0, 0, squareSize2)
 }
 
 function draw() {
   frameRate(10)
+  console.log(c1, c2)
 
   whiteSquare()
   squareSize1 += 15
+
+
+
  
   if(squareSize1 > width) {
     blackSquare()
@@ -36,5 +40,10 @@ function draw() {
     if(squareSize1 && squareSize2 > width) {
       squareSize1 = 100
       squareSize2 = 100
+      c1 -= 20
+      c2 += 20
     }
+
+    
+    
 }
