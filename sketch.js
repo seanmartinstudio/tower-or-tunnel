03e2ea921f039897421253e9479d839ef543a66c
Sean Martin <sean@seanmartinstudio.com>
2023-03-23 12:54:55 -0700

    cleans up comments

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let squareSize1 = 100;
 let squareSize2 = 100;
+let squareSize3 = 100;
 let lineX = 100;
 let lineY = 100;
 
@@ -9,11 +10,18 @@ function setup() {
   background(0);
 }
 
-function whiteSquare() {
+function greenSquare() {
   noFill()
   strokeWeight(4)
   stroke(color(0, 255, 0))
   square(0, 0, squareSize1)
+}
+
+function whiteSquare() {
+  noFill()
+  strokeWeight(4)
+  stroke(color(255))
+  square(0, 0, squareSize3)
 }
 
 function blackSquare() {
@@ -22,19 +30,21 @@ function blackSquare() {
   stroke(color(0))
   square(0, 0, squareSize2)
 
+  //fill in x axis with green as squares progress
   strokeWeight(4)
   stroke(0, 255, 0)
   line(0, 0, 0, lineY)
 
+  //fill in y axis with green as squares progress
   strokeWeight(4)
   stroke(0, 255, 0)
   line(lineX, 0, 0, 0)
 }
 
 function draw() {
-  frameRate(10)
+  frameRate(13)
 
-  whiteSquare()
+  greenSquare()
   squareSize1 += 15
 
   if(squareSize1 > width) {
@@ -42,21 +52,22 @@ function draw() {
     squareSize2 += 15
     lineX += 17
     lineY += 17
-  
   }
 
     if(squareSize1 && squareSize2 > width) {
       squareSize1 = 100
       squareSize2 = 100
+      squareSize3 = 100
       strokeWeight(4)
       stroke(0)
       line(100, 0, 600, 0)
       line(0, 100, 0, 600)
-      
-    }
+     }    
+}
 
-
-
-    
-    
+function keyPressed() {
+  // download 30-second GIF of animation
+  if (key === 's') {
+    saveGif('mySketch', 30);
+  }
 }
